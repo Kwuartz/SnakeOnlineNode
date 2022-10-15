8434773a457df95c14e46984aef4c712e81b058b
Daniyal Rai <daniyalrai364@gmail.com>
2022-10-15 14:00:01 +0100

    Initial commit - rearranged version of original snake repo

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ const players = [];
 const usernames = [];
 let game;
 
-app.use(express.static(__dirname + '/public/'));
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
